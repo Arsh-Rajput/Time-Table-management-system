8534697fd29d3bc54b05d1a8e14b5213d03e6a68
Arsh Rajput <nikhildgrt.nis@gmail.com>
2019-05-30 13:14:42 +0530

    Add files via upload

---
+++ ajaxtest/includes/footer.php
@@ -0,0 +1,50 @@
+<style>
+#footer
+{
+position:relative;
+height:15%;
+width:100%;
+color:white;
+background-color:black;
+font-family:Calibri Light;
+bottom:0px;
+border-top:1px solid white;
+
+}
+#foottable tr td
+{
+	color:white;
+}
+#foottable tr
+{
+	background-color:black;
+}
+
+</style>
+<div id="footer">
+<table id="foottable" width="99%" height="100%" color="inherit">
+<tr color="white">
+	<td width="33.3%" color="white">Our Mentor<br>
+	Mr. Neelendra Baadal.<br>
+	Head Of Department<br>
+	Computer Science<br>
+	
+	</td>
+	<td style="padding:5px;width:33%;color:white;border-left:1px solid white;border-right:1px solid white;">
+	Team Leader: Arsh Rajput<span style="position:relative;right:-250px;"><a style="color:chartreuse;decortion:none;" href="http://localhost/ajaxtest/aboutus.php">Know more</a></span><br>
+	Team Member#1: Anupam Kaushal<br>
+	Team Member#2: Aradhana Mishra<br>
+	Team Member#3: Ankush Dhaak<br>
+	
+	
+	
+	</td>
+	<td width="33%" color="white">Kamla Nehru Institute of Technology,Sultanpur<br>
+	Pin:208118<br>
+	Website:www.knit.ac.in
+	
+	
+	</td>
+</tr>
+</table>
+</div>
